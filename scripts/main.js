500d30633cd5134c944611023a22fac4aef4ac7a
Gitspert25 <muhammad@lilla.family>
2025-04-13 13:37:17 +0500

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+function renderCode() {
+  const code = document.getElementById('codeEditor').value;
+  const output = document.getElementById('output');
+  
+  // Simplified Example - Injecting raw HTML for now
+  output.innerHTML = code; 
+}
